06c29e80015a75eb7c0217e503ffe8f45242def6
760745328 <760745328@qq.com>
2019-06-17 15:49:18 +0800

    wei

---
+++ wei/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+# def home(request):
+#     return HttpResponse('hello')
+def home(request):
+    return render(request, 'home.html')
+def count(request):
+    text = request.GET['text']
+    print(text)
+    result = {}
+    for i in text:
+        if i not in result:
+            result[i] = 1
+        else:
+            result[i] += 1
+    print(result)
+    return render(request, 'count.html', {'count_result': result.items()})
